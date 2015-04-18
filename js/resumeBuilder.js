4d2a86e9d359b03652e043ee01a040d7cacb44dd
Abhishek Chakraborty <abhishekchak93@gmail.com>
2015-04-18 15:30:51 +0530

    temporary commit
    lesson-2 resumeBuilder.js update

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,23 +1,6 @@
-//$("#main").append("Abhishek")
-//var awesomeThoughts;
-//awesomeThoughts = "I am Abhishek and I am AWESOME!!!";
-//console.log(awesomeThoughts);
-//var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-//console.log(funThoughts);
-//$("#main").append(funThoughts);
-
-//var name = "Abhishek Chakraborty";
-//var role = "Web Developer";
-//var formattedName = HTMLheaderName.replace("%data%", name);
-//var formattedRole = HTMLheaderRole.replace("%data%", role);
-//$("#header").prepend(formattedRole);
-//$("#header").prepend(formattedName);
-
-/*
-var skills = ["awesomeness", "programming", "web designing"]
 var bio = {
-	"name" : name,
-	"role" : role,
+	"name" : "Abhishek Chakraborty",
+	"role" : "Web Developer",
 	"contacts":{
 		"mobile" : "(+91)9958345723",
 		"email" : "eofsentinel@outlook.com",
@@ -27,56 +10,96 @@ var bio = {
 	},
 	"bioPic" : "images/me.jpg",
 	"welcomeMsg" : "Hello buddy!",
-	"skills" : skills
+	"skills" : ["awesomeness", "programming", "web designing"]
 }
-*/
 
-/*
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
-var formattedSkills = HTMLskills.replace("%data%", bio.skills);
-*/
-
-/*
-$("#header").prepend(formattedSkills);
-$("#header").prepend(HTMLskillsStart);
-$("#header").prepend(formattedWelcomeMsg);
-$("#header").prepend(formattedBioPic);
-$("#header").prepend(formattedMobile);
-$("#header").prepend(formattedEmail);
-$("#header").prepend(formattedGithub);
-$("#header").prepend(formattedTwitter);
-$("#header").prepend(formattedLocation);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-*/
-
-/*
 var work = {
-	"employer" : "Galgotias College of Engg & Technology"
+	"jobs" : [
+	{
+		"employer" : "Galgotias College of Engg & Technology",
+		"title" : "Student",
+		"dates" : "2012 - 2016",
+		"location" : "Greater Noida",
+		"description" : "No special jobs. Just a student."
+	},
+	{
+		"employer" : "Microsoft",
+		"title" : "Web Developer",
+		"dates" : "2016 - Future",
+		"location" : "San Francsico",
+		"description" : "Lead web developer in the team that manages the global Microsoft webpage."
+	}]
 }
-work.title = "Student";
-work.dates = "2012-2016";
-work.location = "Greater Noida";
 
 var education = {
-	"name" : "Tulsi Vidya Niketan"
+	"schools" : [
+	{
+		"name" : "Tulsi Vidya Niketan",
+		"city" : "Varanasi, UP, India",
+		"years" : "2000-2012",
+		"major" : ["Computer Science", "Physics", "Chemistry", "Mathematics", "English"],
+		"url" : "http://www.tvnschool.org/"
+	},
+	{
+		"name" : "Galgotias College of Engg & Technology",
+		"city" : "Greater Noida, UP, India",
+		"years" : "2012-2016",
+		"major" : "Computer Science",
+		"url" : "http://galgotiacollege.edu/"
+	}],
+	"onlineCourses" : [
+	{
+		"title" : "JavaScript Basics",
+		"url" : "https://www.udacity.com/course/ud804"
+	},
+	{
+		"title" : "Introduction to Programming using Python",
+		"url" : "https://www.edx.org/course/introduction-computer-science-mitx-6-00-1x-0"
+	},
+	{
+		"title" : "Algorithms: Design & Analysis, Part 1",
+		"url" : "https://www.coursera.org/course/algo"
+	}]
 }
-education["degree"] = "Intermediate";
-education["dates"] = "2000-2012";
-education["location"] = "Varanasi, India";
-education["major"] = "Physical Sciences";
-*/
 
-/*
-$("#header").prepend(work.title);
-$("#header").prepend(education.name);
-*/
+var projects = {
+	"projects" : [
+	{
+		"title" : "Atari Breakout",
+		"dates" : "2013",
+		"description" : "A simple version of the famous Atari Breakout game, implemented in C using Stanford Portable Library. Currently supports Linux-based OS only.",
+		"image" : "images/projectBreakout"
+	},
+	{
+		"title" : "Simple Chess",
+		"dates" : "2015",
+		"description" : "A simple version of Chess game, implemented in Python. For Windows OS. Currently under development.",
+		"image" : ""
+	}]
+}
+
+if (bio["skills"].length > 0){
+	$("#header").append(HTMLskillsStart);
+	for (i in bio["skills"])
+	{
+		var formattedSkill = HTMLskills.replace("%data%", bio["skills"][i]);
+		$("#skills").append(formattedSkill);
+	}
+}
+
+if (work["jobs"].length > 0){
+	$("#workExperience").append(HTMLworkStart);
+	for (i in work["jobs"])
+	{
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work["jobs"][i]["employer"]);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work["jobs"][i]["title"]);
+		$(".work-entry:last").append(formattedEmployer + formattedTitle);
+		var formattedDates = HTMLworkDates.replace("%data%", work["jobs"][i]["dates"]);
+		$(".work-entry:last").append(formattedDates);
+		var formattedLocation = HTMLworkLocation.replace("%data%", work["jobs"][i]["location"]);
+		$(".work-entry:last").append(formattedLocation);
+		var formattedDescription = HTMLworkDescription.replace("%data%", work["jobs"][i]["description"]);
+		$(".work-entry:last").append(formattedDescription);
+	}
+}
 
