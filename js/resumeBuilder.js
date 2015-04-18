718daadc507400a9f7a36e5dd8885b758b3313e0
Abhishek Chakraborty <abhishekchak93@gmail.com>
2015-04-18 11:16:23 +0530

    temporary commit
    new image added, modifications to resumeBuilder.js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,82 @@
+//$("#main").append("Abhishek")
+//var awesomeThoughts;
+//awesomeThoughts = "I am Abhishek and I am AWESOME!!!";
+//console.log(awesomeThoughts);
+//var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+//console.log(funThoughts);
+//$("#main").append(funThoughts);
+
+//var name = "Abhishek Chakraborty";
+//var role = "Web Developer";
+//var formattedName = HTMLheaderName.replace("%data%", name);
+//var formattedRole = HTMLheaderRole.replace("%data%", role);
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
+
+/*
+var skills = ["awesomeness", "programming", "web designing"]
+var bio = {
+	"name" : name,
+	"role" : role,
+	"contacts":{
+		"mobile" : "(+91)9958345723",
+		"email" : "eofsentinel@outlook.com",
+		"github" : "abhichak95",
+		"twitter" : "abhichak93",
+		"location" : "Greater Noida"
+	},
+	"bioPic" : "images/me.jpg",
+	"welcomeMsg" : "Hello buddy!",
+	"skills" : skills
+}
+*/
+
+/*
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+*/
+
+/*
+$("#header").prepend(formattedSkills);
+$("#header").prepend(HTMLskillsStart);
+$("#header").prepend(formattedWelcomeMsg);
+$("#header").prepend(formattedBioPic);
+$("#header").prepend(formattedMobile);
+$("#header").prepend(formattedEmail);
+$("#header").prepend(formattedGithub);
+$("#header").prepend(formattedTwitter);
+$("#header").prepend(formattedLocation);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+*/
+
+/*
+var work = {
+	"employer" : "Galgotias College of Engg & Technology"
+}
+work.title = "Student";
+work.dates = "2012-2016";
+work.location = "Greater Noida";
+
+var education = {
+	"name" : "Tulsi Vidya Niketan"
+}
+education["degree"] = "Intermediate";
+education["dates"] = "2000-2012";
+education["location"] = "Varanasi, India";
+education["major"] = "Physical Sciences";
+*/
+
+/*
+$("#header").prepend(work.title);
+$("#header").prepend(education.name);
+*/
+
